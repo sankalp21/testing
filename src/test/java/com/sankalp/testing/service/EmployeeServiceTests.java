228b776b3eeda2af9e2b78840c8ad2add6a1dd5f
Sankalp Sinha <sankalpsinha7@gmail.com>
2022-08-24 19:41:33 +0530

    added tests for getting all employees

--- src/test/java/com/sankalp/testing/service/EmployeeServiceTests.java
+++ src/test/java/com/sankalp/testing/service/EmployeeServiceTests.java
@@ -4,7 +4,11 @@ import com.sankalp.testing.exceptions.ResourceNotFoundException;
 import com.sankalp.testing.model.Employee;
 import com.sankalp.testing.repository.EmployeeRepository;
 import com.sankalp.testing.service.impl.EmployeeServiceImpl;
-import org.assertj.core.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -48,11 +52,11 @@ public class EmployeeServiceTests {
                 .willReturn(Optional.empty());
         given(employeeRepository.save(employee)).willReturn(employee);
 
-        //when ->  save employee
+        //when ->  behavior that we need to test
         Employee savedEmployee = employeeService.saveEmployee(employee);
 
         //then -> verify results
-        Assertions.assertThat(savedEmployee).isNotNull();
+        assertThat(savedEmployee).isNotNull();
 
     }
 
@@ -63,7 +67,7 @@ public class EmployeeServiceTests {
         given(employeeRepository.findByEmail(employee.getEmail()))
                 .willReturn(Optional.of(employee));
 
-        //when ->  save employee
+        //when ->  behavior that we need to test
         org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, ()->{
             employeeService.saveEmployee(employee);
         });
@@ -71,5 +75,44 @@ public class EmployeeServiceTests {
 
         //then -> verify results
         verify(employeeRepository, never()).save(any(Employee.class));
+    }
+
+    //Junit for getting all employee
+    @Test
+    public void givenEmployeeList_whenGetAllEmployee_thenReturnEmployeeList(){
+        //given -> precondition or setup
+        Employee employee1 = Employee.builder()
+                .firstName("Joe")
+                .lastName("ro")
+                .email("joe.ro@mail.com")
+                .build();
+
+        List<Employee> returnResult = new ArrayList<>();
+
+        returnResult.add(employee);
+        returnResult.add(employee1);
+
+        given(employeeRepository.findAll()).willReturn(returnResult);
+
+        //when ->  behavior that we need to test
+        List<Employee> employeeList = employeeService.getAllEmployee();
+
+        //then -> verify results
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(2);
+    }
+
+    //Junit for getting all employee negative scenario
+    @Test
+    public void givenEmptyEmployeeList_whenGetAllEmployee_thenReturnEmptyEmployeeList(){
+        //given -> precondition or setup
+        given(employeeRepository.findAll()).willReturn(Collections.emptyList());
+
+        //when ->  behavior that we need to test
+        List<Employee> employeeList = employeeService.getAllEmployee();
+
+        //then -> verify results
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList.size()).isEqualTo(0);
     }
 }
