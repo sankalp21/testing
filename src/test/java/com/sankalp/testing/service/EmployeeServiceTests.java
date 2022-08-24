a48f12917760f2f529b9fdc754009447a298655b
Sankalp Sinha <sankalpsinha7@gmail.com>
2022-08-24 19:06:20 +0530

    added exception tests for save employee

--- src/test/java/com/sankalp/testing/service/EmployeeServiceTests.java
+++ src/test/java/com/sankalp/testing/service/EmployeeServiceTests.java
@@ -1,5 +1,6 @@
 package com.sankalp.testing.service;
 
+import com.sankalp.testing.exceptions.ResourceNotFoundException;
 import com.sankalp.testing.model.Employee;
 import com.sankalp.testing.repository.EmployeeRepository;
 import com.sankalp.testing.service.impl.EmployeeServiceImpl;
@@ -7,7 +8,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -34,7 +40,7 @@ public class EmployeeServiceTests {
                 .build();
     }
 
-    //Junit for saveEmoloyee using mock()
+    //Junit for saveEmoloyee
     @Test
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployee(){
         //given -> precondition or setup
@@ -42,11 +48,28 @@ public class EmployeeServiceTests {
                 .willReturn(Optional.empty());
         given(employeeRepository.save(employee)).willReturn(employee);
 
-
         //when ->  save employee
         Employee savedEmployee = employeeService.saveEmployee(employee);
 
         //then -> verify results
         Assertions.assertThat(savedEmployee).isNotNull();
+
+    }
+
+    //Jnuit for saveEmployee where exception is thrown
+    @Test
+    public void givenExistingEmployeeObject_whenSaveEmployee_thenThrowException(){
+        //given -> precondition or setup
+        given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+
+        //when ->  save employee
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFoundException.class, ()->{
+            employeeService.saveEmployee(employee);
+        });
+
+
+        //then -> verify results
+        verify(employeeRepository, never()).save(any(Employee.class));
     }
 }
