a25999e93e750e7471d032200c06f8bc3af66d93
Sankalp Sinha <sankalpsinha7@gmail.com>
2022-08-24 19:43:09 +0530

    added getEmployee by id method in service

--- src/main/java/com/sankalp/testing/service/EmployeeService.java
+++ src/main/java/com/sankalp/testing/service/EmployeeService.java
@@ -3,9 +3,11 @@ package com.sankalp.testing.service;
 import com.sankalp.testing.model.Employee;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeService {
    
     public Employee saveEmployee(Employee employee);
     public List<Employee> getAllEmployee();
+    public Optional<Employee> getEmployeeById(long id);
 }
